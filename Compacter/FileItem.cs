3702371f2574f4b7a18ccb9c7dcc551155da83ba
Hans Isbrücker <hisbrucker@gmail.com>
2023-05-22 11:50:42 +0200

    Added FileItem class

--- Compacter/FileItem.cs
+++ Compacter/FileItem.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 
 namespace Compacter
 {
-    internal class FolderManager
+    internal class FileItem
     {
         public required string Path { get; init; }
 
-
-        
+        public void Analyze() 
+        {
+            // TODO size on disk
+            // TODO is compressed?
+            // TODO entropy
+        }
     }
 }
