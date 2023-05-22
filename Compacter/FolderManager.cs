569b83627b996c418353fca2fcc086b3809aff30
Hans Isbrücker <hisbrucker@gmail.com>
2023-05-22 11:39:35 +0200

    Add FolderManager class

---
+++ Compacter/FolderManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compacter
+{
+    internal class FolderManager
+    {
+        public required string Path { get; init; }
+
+        public FolderManager(string path)
+        {
+            Path = path;
+        }
+    }
+}
