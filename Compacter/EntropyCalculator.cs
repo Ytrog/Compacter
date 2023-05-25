94f5a9865188c0c746ba97a70abdf04dc2d17db9
Hans Isbrücker <hisbrucker@gmail.com>
2023-05-25 10:56:16 +0200

    Made static methods public for testability

--- Compacter/EntropyCalculator.cs
+++ Compacter/EntropyCalculator.cs
@@ -27,7 +27,7 @@ namespace Compacter
         /// <param name="fileLength">The total length of the file</param>
         /// <param name="count">The statistics per byte (it is an array of length 0xFF with the count per byte value)</param>
         /// <returns></returns>
-        private static double CalculateEntropy(long fileLength, uint[] count)
+        public static double CalculateEntropy(long fileLength, uint[] count)
         {
             double entropy = 0;
             double temp;
@@ -45,7 +45,7 @@ namespace Compacter
             return entropy;
         }
 
-        private static uint[] CountBytes(FileInfo fileInfo)
+        public static uint[] CountBytes(FileInfo fileInfo)
         {
             uint[] byteCount = new uint[0xFF]; // create an array with the count for each byte value (from 0x00 to 0xFF)
 
