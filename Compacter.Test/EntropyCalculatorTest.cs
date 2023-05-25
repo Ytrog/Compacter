e70c00e59ec857c276ca4dc58470a290f6fee2a4
Hans Isbrücker <hisbrucker@gmail.com>
2023-05-25 11:47:03 +0200

    Create methods to test the scenario if the file is all different

--- Compacter.Test/EntropyCalculatorTest.cs
+++ Compacter.Test/EntropyCalculatorTest.cs
@@ -21,5 +21,39 @@ namespace Compacter.Test
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void CalculateEntropyAllDifferent()
+        {
+            uint[] count = new uint[0xFF];
+            
+            for (int i = 0; i < count.Length; i++) 
+            {
+                count[i] = 1;
+            }
+
+            double expected = 8;
+
+            double actual = EntropyCalculator.CalculateEntropy(count.Length, count);
+
+            Assert.AreEqual((byte)expected, actual);
+        }
+
+        /// <summary>
+        /// Generate a file for comparison
+        /// </summary>
+        [TestMethod]
+        [Ignore]
+        public void GenerateFile()
+        {
+            byte[] buffer = new byte[0xFF];
+
+            for(int i = 0;i < buffer.Length; i++)
+            {
+                buffer[i] = (byte)i;
+            }
+
+            File.WriteAllBytes(@"C:\test\entropytest.dat", buffer);
+        }
     }
 }
