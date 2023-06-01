13239085be420c8b6754f345cd161d63a210d567
Hans Isbrücker <hisbrucker@gmail.com>
2023-06-01 12:07:21 +0200

    Add a test for the compressor to hunt a bug

---
+++ Compacter.Test/CompressorTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compacter.Test
+{
+    [TestClass]
+    public class CompressorTest
+    {
+        /// <summary>
+        /// Explore what GetExtension does
+        /// </summary>
+        [TestMethod]
+        [Ignore]
+        public void CompressorCorrectlySeesExecutables()
+        {
+            string actual = Path.GetExtension(@"C:\Users\ABCdeCirkel\.vscode\extensions\rust-lang.rust-analyzer-0.3.1506-win32-x64\server\rust-analyzer.exe");
+
+            string expected = "exe";
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
