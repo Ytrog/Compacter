d81c83898bd4d01618f4cd074986263536b24330
Hans Isbrücker <hisbrucker@gmail.com>
2023-06-21 13:08:56 +0200

    Made an icon at 3 resolutions

--- Compacter/Properties/Resources.Designer.cs
+++ Compacter/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace Compacter.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Compressor {
+            get {
+                object obj = ResourceManager.GetObject("Compressor", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
